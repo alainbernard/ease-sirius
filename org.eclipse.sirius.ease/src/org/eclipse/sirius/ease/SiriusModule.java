496a4ac6d7e17f5165c6562b9e2786aa1f6584a8
Alain BERNARD <alain.bernard1224@gmail.com>
2017-06-06 16:34:51 +0200

    Initial commit

---
+++ org.eclipse.sirius.ease/src/org/eclipse/sirius/ease/SiriusModule.java
@@ -0,0 +1,15 @@
+package org.eclipse.sirius.ease;
+
+import org.eclipse.ease.modules.AbstractScriptModule;
+import org.eclipse.ease.modules.WrapToScript;
+
+public class SiriusModule extends AbstractScriptModule {
+
+	public SiriusModule() {
+		// TODO Auto-generated constructor stub
+	}
+	
+//	@WrapToScript
+//	public DRe
+
+}
