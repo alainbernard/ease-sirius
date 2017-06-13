204e962874df5300d9491149ae4e9759ffd86e71
Alain <alain.bernard1224@gmail.com>
2017-06-13 21:30:35 +0200

    Add some help and functions

--- org.eclipse.sirius.ease/src/org/eclipse/sirius/ease/SiriusModule.java
+++ org.eclipse.sirius.ease/src/org/eclipse/sirius/ease/SiriusModule.java
@@ -1,9 +1,7 @@
 package org.eclipse.sirius.ease;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -15,45 +13,69 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.ease.modules.AbstractScriptModule;
 import org.eclipse.ease.modules.WrapToScript;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
-import org.eclipse.sirius.business.api.dialect.command.CreateRepresentationCommand;
 import org.eclipse.sirius.business.api.modelingproject.ModelingProject;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.ext.base.Option;
 import org.eclipse.sirius.ui.business.api.dialect.DialectUIManager;
-import org.eclipse.sirius.ui.business.api.session.EditingSessionEvent;
-import org.eclipse.sirius.ui.business.api.session.IEditingSession;
-import org.eclipse.sirius.ui.business.api.session.SessionUIManager;
 import org.eclipse.sirius.ui.business.api.viewpoint.ViewpointSelectionCallback;
 import org.eclipse.sirius.ui.business.internal.commands.ChangeViewpointSelectionCommand;
 import org.eclipse.sirius.ui.tools.api.project.ModelingProjectManager;
 import org.eclipse.sirius.viewpoint.DAnalysis;
 import org.eclipse.sirius.viewpoint.DRepresentation;
 import org.eclipse.sirius.viewpoint.DView;
-import org.eclipse.sirius.viewpoint.description.RepresentationDescription;
 import org.eclipse.sirius.viewpoint.description.Viewpoint;
 import org.eclipse.ui.actions.WorkspaceModifyOperation;
 
+/**
+ * A module that provides high-level Sirius functions to be used in any language supported by EASE. 
+ * @author A. Bernard 
+ *
+ */
 public class SiriusModule extends AbstractScriptModule {
 
-
-
+	/**
+	 * Convert the given project to a {@link ModelingProject}. This should be called somewhere in 
+	 * a {@link WorkspaceModifyOperation}.
+	 * @param project
+	 * @return the project as a {@link ModelingProject}.
+	 * @throws CoreException if an error occurs
+	 */
 	@WrapToScript
-	public void toto() {
-		Set<Viewpoint> viewpoints = ViewpointRegistry.getInstance().getViewpoints();
-		for (Viewpoint v : viewpoints) {
-			v.getModelFileExtension();
+	public ModelingProject configureToModeling(IProject project) throws CoreException {
+		ModelingProjectManager.INSTANCE.convertToModelingProject(project, new NullProgressMonitor());
+		Option<ModelingProject> modelingProject = ModelingProject.asModelingProject(project);
+		return modelingProject.get();
+	}
+	
+	/**
+	 * Get the {@link Viewpoint} that can be applied to the provided model file. It relies on the file extension 
+	 * to compute the available viewpoints.
+	 * @param modelFile
+	 * @return a collection of viewpoints
+	 */
+	@WrapToScript
+	public Set<Viewpoint> getViewpointsFor(IFile modelFile) {
+		Set<Viewpoint> designViewpoints = new HashSet<>();
+		String fileExtn = modelFile.getFileExtension();
+		for (Viewpoint v : ViewpointRegistry.getInstance().getViewpoints()) {
+			if (v.getModelFileExtension().equalsIgnoreCase(fileExtn)) {
+				designViewpoints.add(v);
+			}
 		}
+		return designViewpoints;
 	}
 
-	
-
+	/**
+	 * Create a Sirius {@link Session} for the provided model file. It uses the first available {@link Viewpoint} that 
+	 * can be applied to the model file and applies it. If <code>initialization</code> is set to <code>true</code> in 
+	 * the representation definition, then Sirius will create automatically.
+	 * @param modelFile the file containing the EMF model
+	 * @return the created Sirius session
+	 * @throws InvocationTargetException
+	 * @throws InterruptedException
+	 */
 	@WrapToScript
 	public Session createSession(IFile modelFile) throws InvocationTargetException, InterruptedException {
 
@@ -65,9 +87,14 @@ public class SiriusModule extends AbstractScriptModule {
 		DRepresentation myRepresentation = dView.getOwnedRepresentationDescriptors().get(0).getRepresentation();
 		DialectUIManager.INSTANCE.openEditor(siriusSession, myRepresentation, new NullProgressMonitor());
 		return operation.getCreatedSession();
-
-
-
+	}
+	
+	@WrapToScript
+	public DRepresentation getRepresentation(Session siriusSession) {
+		DAnalysis root = (DAnalysis) siriusSession.getSessionResource().getContents().get(0);
+		DView dView = root.getOwnedViews().get(0);
+		DRepresentation myRepresentation = dView.getOwnedRepresentationDescriptors().get(0).getRepresentation();
+		return myRepresentation;
 	}
 
 
